3148b3ae5e693c23642353211392e327200efa2f
Steeve Jerent <jerent.steeve@gmail.com>
2015-09-28 18:04:21 +0200

    Ajout de IO socket

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-var io = require('socket.io')
 var express = require('express')
 var app = express()
 var http = require('http').Server(app)
+var io = require('socket.io')(http)
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/views/index.html')
 })
-
+io.on('connection', function(socket){
+	console.log('a user connected')
+})
 http.listen(3000, function(){
 	console.log('listen on *:3000')
 })
